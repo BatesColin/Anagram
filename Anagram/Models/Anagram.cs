1e22f18359b5ac537da22c89f6d658f725d76110
Colin Bates and Jean Jia <jean@>
2018-06-27 11:38:43 -0700

    Writing loops.

--- Anagram/Models/Anagram.cs
+++ Anagram/Models/Anagram.cs
@@ -1,12 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Anagram
 {
-public class Anagram
+  public class Anagram
   {
-  public bool ExampleTest(int year)
+    public static string Comparision(string wordList, string inputWord)
     {
-      // logic will go here
-      return false;
+      // string[] characters = inputWord.Sort();
+      string sortedWord = String.Concat(inputWord.OrderBy(c=>c));
+      string[] wordCheck = wordList.Split(' ');
+      foreach (string word in wordCheck)
+      {
+        if (inputWord.Length == word.Length)
+        {
+          string newWord = String.Concat(word.OrderBy(c=>c));
+          if(sortedWord == newWord)
+          {
+            return word;
+          }
+        }
+        else
+        {
+          return Console.WriteLine("Not an anagram");
+        }
+      }
+    }
+ //"hat" ["tha" "bat" "the" "this"] = sorted
+    public static void Main()
+    {
+      Console.WriteLine("Input an initial word.");
+      public string inputWord = Console.ReadLine();
+      Console.WriteLine("Input a list of possible anagram words");
+      public string wordList = Console.ReadLine();
     }
   }
 }
-  
---
+++ README.md
@@ -0,0 +1,41 @@
+# _Anagram_
+
+#### _This is an anagram app. June 27, 2018_
+
+#### By _**Colin Bates and Jean Jia**_
+
+## Description
+
+_This app was created to compare a word and display if it is an anagram.
+
+Specs:
+1. User may input multiple words.
+2. Will compare the length of words.
+3. Words will be sorted.
+4. Letters will be compare for matches.
+5. App will display wether it is an anagram.
+_
+
+## Setup/Installation Requirements
+
+* _Clone the folder_
+* _Run the app and follow the prompt_
+
+## Known Bugs
+
+_No known bugs._
+
+## Support and contact details
+
+_Colin Bates email: dragonflylord@gmail.com_
+_Jean Jia email: Jean84646@gmail.com_
+
+## Technologies Used
+
+_Created with Atom editor.  Use any browser to view._
+
+### License
+
+*No license required*
+
+Copyright (c) 2018 **_Colin Bates and Jean Jia_**
