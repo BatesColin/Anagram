06368f63d81fdb8a243314392e676d3dc4e2bb63
Colin Bates and Jean Jia <colin@>
2018-06-27 16:36:03 -0700

    Fix some errors.

--- Anagram/Models/Anagram.cs
+++ Anagram/Models/Anagram.cs
@@ -10,15 +10,15 @@ namespace Anagram
     {
 
     }
-    public static string Comparision(string wordList, string inputWord)
+    public string Comparision(string wordList, string inputWord)
     {
-      public string nope = "Not an anagram";
-      // string[] characters = inputWord.Sort();
+      // string nope = "Not an anagram";
       string sortedWord = String.Concat(inputWord.OrderBy(c=>c));
       string[] wordCheck = wordList.Split(' ');
-      foreach (string word in wordCheck)
       List<string> result=new List<string>{};
+      foreach (string word in wordCheck)
       {
+
         if (inputWord.Length == word.Length)
         {
           string newWord = String.Concat(word.OrderBy(c=>c));
@@ -27,22 +27,23 @@ namespace Anagram
             result.Add(word);
           }
         }
+        //Assert.Fail("Not an anagram");
       }
-    }
       string[] tempArray = result.ToArray();
       string anagramWord = tempArray.ToString();
       return anagramWord;
     }
+  }
   public class Program
   {
     public static void Main()
     {
       Anagram myAnagram = new Anagram();
       Console.WriteLine("Input an initial word.");
-      public string inputWord = Console.ReadLine();
+      string inputWord = Console.ReadLine();
       Console.WriteLine("Input a list of possible anagram words");
-      public string wordList = Console.ReadLine();
-      string result = myAnagram.Comparision(wordList, inputWord)
+      string wordList = Console.ReadLine();
+      string result = myAnagram.Comparision(wordList, inputWord);
       Console.WriteLine(anagramWord);
     }
   }
